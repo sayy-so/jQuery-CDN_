de2dd29f2b5d1c94be0de7622ebb658bf8824c59
sayy__so <nurgulbotobaeva25@gmail.com>
2023-02-01 15:48:54 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,92 @@
+                    //~jQuery CDN
+
+// hide()               //·display: none
+// show()               //·display: block
+// fageIn()             //·display: block
+// fadeOut()            //·display: none
+// slideDown()          //·display: block
+// slideUp()            //·display: none
+// slideToggle()        //·dislplay == block =>display = none
+                        //·display == none => display = block
+
+
+//1
+// $('.block').fadeIn(3000)
+// $('.block').fadeOut(4000)
+
+//2
+// $('.block').slideDown(2000)
+// $('.block').slideUp(3000)
+
+//3
+// $('.block').slideToggle(2000)
+
+
+//Задачи
+
+//1
+
+$('#btn').click(()=>{
+    $('.box1').hide(1000)
+$('.box2').hide(2000)
+$('.box3').hide(2500)
+$('.box4').hide(3000)
+$('.box5').hide(3500)
+
+
+
+$('.box1').show(4000)
+$('.box2').show(3000)
+$('.box3').show(2000)
+$('.box4').show(1700)
+$('.box5').show(1000)
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+//2
+// $('.box1').fadeOut(1000)
+// $('.box2').fadeOut(2000)
+// $('.box3').fadeOut(3000)
+// $('.box4').fadeOut(4000)
+// $('.box5').fadeOut(5000)
+
+// $('.box1').fageIn(6000)
+// $('.box2').fageIn(7000)
+// $('.box3').fageIn(8000)
+// $('.box4').fageIn(9000)
+// $('.box5').fageIn(10000)
+
+
+
+
+//3
+// $('.box1').slideUp(1000)
+// $('.box2').slideUp(1200)
+// $('.box3').slideUp(1400)
+// $('.box4').slideUp(1600)
+// $('.box5').slideUp(1800)
+
+// $('.box1').slideDown(1500)
+// $('.box2').slideDown(1700)
+// $('.box3').slideDown(1900)
+// $('.box4').slideDown(2200)
+// $('.box5').slideDown(2400)
+
+
+
+
+
+
